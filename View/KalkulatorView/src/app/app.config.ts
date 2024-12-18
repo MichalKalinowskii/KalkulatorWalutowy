925475bca08dcab630fee7a6a6d170217946484c
Michal Kalinowski <kalinowskimichal638@gmail.com>
2024-12-18 23:11:11 +0100

    calculator further implementation

--- View/KalkulatorView/src/app/app.config.ts
+++ View/KalkulatorView/src/app/app.config.ts
@@ -6,13 +6,15 @@ import { provideHttpClient } from '@angular/common/http';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
   providers: [ 
     [provideRouter(routes)],
     provideHttpClient(), 
+    provideAnimations(),
     {provide: MAT_DATE_LOCALE, useValue: 'pl-PL'},
     BrowserModule,
-    provideAnimations() 
+    FormsModule
   ]
 };
--- View/KalkulatorView/src/app/view/calculator/calculator.component.ts
+++ View/KalkulatorView/src/app/view/calculator/calculator.component.ts
@@ -1,14 +1,61 @@
-import { Component, Input, input } from '@angular/core';
-import { NBPRates } from '../../models/NBPRates';
+import { AfterViewInit, Component, Input, OnChanges } from '@angular/core';
 import { Rates } from '../../models/rates';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.css'
 })
-export class CalculatorComponent {
+export class CalculatorComponent implements AfterViewInit, OnChanges {
   @Input() public rates!: Rates[];
+
+  public leftInput!: number;
+  public rightInput!: number;
+
+  public leftRate!: Rates;
+  public rightRate!: Rates;
+  
+  ngAfterViewInit(): void {
+    this.rates.unshift( { code: 'PLN', mid: 1, currency: "złoty" } as Rates );
+    this.leftRate = this.rates[0];
+    this.rightRate = this.rates.find(rate => rate.code === 'EUR') as Rates;
+  }
+
+  ngOnChanges(): void {
+    this.rates.unshift( { code: 'PLN', mid: 1, currency: "złoty" } as Rates );
+    this.leftRate = this.rates[0];
+    this.rightRate = this.rates.find(rate => rate.code === 'EUR') as Rates;
+    this.onLeftRateChange(this.leftRate);
+  }
+
+  public onLeftRateChange(rate: Rates): void {
+    if (this.leftInput != null) 
+    {
+      if (this.rightInput == null) {
+        this.rightInput = 1;
+      }
+
+      this.rightInput = parseFloat((rate.mid / this.rightRate.mid * this.leftInput).toFixed(4));
+    }
+    else {
+      this.rightInput = 0;
+    }
+  }
+
+  public onRightRateChange(rate: Rates): void {
+    if (this.rightInput != null) 
+    {
+      if (this.leftInput == null) {
+        this.leftInput = 1;
+      }
+
+      this.leftInput = parseFloat((rate.mid / this.leftRate.mid * this.rightInput).toFixed(4));
+    }
+    else {
+      this.leftInput = 0;
+    }
+  }
 }
